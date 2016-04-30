6005c8dc4f2f138fd5b63c2857354a527af769b1
nischalsource <nischalbachu@gmail.com>
2016-04-30 03:23:42 +0200

    Adds handling of positional parameters including a help flag

--- d.sh
+++ d.sh
@@ -1,15 +1,78 @@
 #!/bin/bash
 
+
+#Prints out help on how to use this script
+function  echoHelp () {
+cat <<-END
+Usage:
+------
+   -h | --help
+     Display this help
+   -c | --cookie
+     Add the absolute filesystem location to the Netscape format cookie.txt file
+   -d | --dir
+     Specify the name of the directory this script must create from this level to download 
+   -u | --url
+     Specify the complete URI to download in the following URI format:  protocol://domain/directory
+END
+}
+
+
+
+#Checks for Parameters
+if [ $# -eq 0 ]; then
+    echo "No arguments specified. Try -h for help"
+    exit;
+fi
+       
+
+
+#Processes parameters
+while [ ! $# -eq 0 ]
+do
+    case $1 in
+        -c | --cookie)
+            echo "The cookie value is " $2
+            cookie=$2
+            shift 2 ;;
+        -d | --dir)
+            echo "The directory value is " $2
+            dir=$2
+            shift 2 ;;
+        -u | --url)
+            echo "The url is " $2
+            url=$2
+            shift 2 ;;
+        -h | \? | --help)
+            echoHelp
+            exit
+            ;;
+    esac
+done
+
+
+#Checks for Parameters
+if [ -n ${dir} ]; 
+then
+    echo "Error: Please Specify the name of the directory this script must create from this level to download"
+    exit;
+elif [ -n ${cookie} ]; 
+then
+    echo "Error: Please Specify the name of the directory this script must create from this level to download"
+    exit;
+elif [ -n ${url} ]; 
+then
+    echo "Error: Please specify the complete URI to download in the following URI format:  protocol://domain/directory"
+    exit;
+fi
+
+exit;
+
 domain="https://www.safaribooksonline.com"
 domainLength=${#domain}
 
-cookie=$1
-
-dir=$2
 dir_separator="/"
 dirLength=$((${#dir} + ${#dir_separator}))
-
-url=$3
 
 includePath3=${url:$domainLength}
 includePath2=${includePath3%$dir_separator}

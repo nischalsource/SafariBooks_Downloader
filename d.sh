aa5270e09f240d1ca0732cc9c176f255552aa16b
nischalsource <nischalbachu@gmail.com>
2016-05-01 01:55:58 +0200

    Improves the Processing of parameters

--- d.sh
+++ d.sh
@@ -27,21 +27,21 @@ fi
        
 
 
-#Processes parameters
+#Processes Parameters
 while [ ! $# -eq 0 ]
 do
     case $1 in
         -c | --cookie)
-            echo "The cookie value is " $2
             cookie=$2
+	    echo "The cookie value is " $cookie
             shift 2 ;;
         -d | --dir)
-            echo "The directory value is " $2
-            dir=$2
+	    dir=$2
+            echo "The directory value is: " $dir
             shift 2 ;;
         -u | --url)
-            echo "The url is " $2
             url=$2
+	    echo "The url is: " $url
             shift 2 ;;
         -h | \? | --help)
             echoHelp
@@ -50,23 +50,18 @@ do
     esac
 done
 
-
-#Checks for Parameters
-if [ -n ${dir} ]; 
-then
-    echo "Error: Please Specify the name of the directory this script must create from this level to download"
-    exit;
-elif [ -n ${cookie} ]; 
-then
-    echo "Error: Please Specify the name of the directory this script must create from this level to download"
-    exit;
-elif [ -n ${url} ]; 
-then
-    echo "Error: Please specify the complete URI to download in the following URI format:  protocol://domain/directory"
-    exit;
+#Check Parameters have been successfully set
+if ${dir+"false"}; then
+   echo "Error: Please Specify the name of the directory this script must create from this level to download";
+   exit;
+elif ${cookie+"false"}; then
+   echo "Error: Please Specify the absolute filesystem location to the Netscape format cookie.txt file"
+   exit;
+elif ${url+"false"}; then
+   echo "Error: Please specify the complete URI to download in the following URI format:  protocol://domain/directory"
+   exit;
 fi
 
-exit;
 
 domain="https://www.safaribooksonline.com"
 domainLength=${#domain}
@@ -78,22 +73,19 @@ includePath3=${url:$domainLength}
 includePath2=${includePath3%$dir_separator}
 includePath=${includePath2},/static
 
-echo $cookie
-echo $dir
-echo $url
-echo $includePath
+echo "The includePath is: " $includePath
 
 #Construct Container Directory
 mkdir $dir
 cd $dir
 
-#Main Download
+#Main Download in a recursive way
 wget -k -r --no-directories -I $includePath --header='Host: www.safaribooksonline.com' --header='User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0' --header='Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header='Accept-Language: en-US,en;q=0.5' --header='Content-Type: application/x-www-form-urlencoded' --load-cookies $cookie $url
 
+#Redownload all CSS
 for file in *.css;
 do
 echo $file
-#Redownload all CSS
 wget -O- --header='Accept-Encoding: gzip,deflate,br' --header='Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header='Accept-Language: en-US,en;q=0.5' --header='Content-Type: application/x-www-form-urlencoded' https://www.safaribooksonline.com/static/CACHE/css/$file | gunzip > $file
 done;
 
@@ -103,7 +95,7 @@ do
 mv $xhtmlFile ${xhtmlFile: 0: $((${#xhtmlFile} -6))}.html; 
 done;
 
-#Replace  all in file xhtml links with html links
+#Replace  all in-file xhtml links with html links
 for htmlFile in *.html
 do
 sed -i s/xhtml/html/g $htmlFile
